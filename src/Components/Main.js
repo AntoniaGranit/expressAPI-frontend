691ad71fab3c38cd4848928fedbf1ec1c61286c8
AntoniaGranit <antonia.granit@gmail.com>
2023-05-26 19:24:50 +0300

    change route capitalization in main.js

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -5,8 +5,8 @@ import { Select } from '../Styles/SelectComponent';
 import { MainSection } from '../Styles/Sections';
 
 export const Main = () => {
-  const [selectedLanguage, setSelectedLanguage] = useState('English');
-  const [selectedGender, setSelectedGender] = useState('Female');
+  const [selectedLanguage, setSelectedLanguage] = useState('english');
+  const [selectedGender, setSelectedGender] = useState('female');
   const handleLanguageChange = (event) => {
     console.log(event.target.value);
     setSelectedLanguage(event.target.value);
@@ -19,35 +19,35 @@ export const Main = () => {
   return (
     <MainSection>
       <Select
-        placeholder="Select language"
+        placeholder="select language"
         value={selectedLanguage}
         label="language"
         onChange={handleLanguageChange}>
         <option value="">order by work language:</option>
-        <option value="English">english</option>
-        <option value="German">german</option>
-        <option value="Spanish">spanish</option>
-        <option value="Russian">russian</option>
-        <option value="Polish">polish</option>
+        <option value="english">english</option>
+        <option value="german">german</option>
+        <option value="spanish">spanish</option>
+        <option value="russian">russian</option>
+        <option value="polish">polish</option>
       </Select>
       <Link to={`/professionals/${selectedLanguage}`}>
         <Button type="button" disabled={!selectedLanguage}>sort by work language</Button>
       </Link>
 
       <Select
-        placeholder="Select gender"
+        placeholder="select gender"
         value={selectedGender}
         label="gender"
         onChange={handleGenderChange}>
         <option value="">order by gender:</option>
-        <option value="Female">female</option>
-        <option value="Male">male</option>
+        <option value="female">female</option>
+        <option value="male">male</option>
       </Select>
       <Link to={`/${selectedGender}`}>
         <Button type="button" disabled={!selectedGender}>sort by gender</Button>
       </Link>
-      <NavigationLink to="/RandomProfessional">random professional</NavigationLink>
-      <NavigationLink to="/CompleteList">all professionals</NavigationLink>
+      <NavigationLink to="/randomprofessional">random professional</NavigationLink>
+      <NavigationLink to="/completelist">all professionals</NavigationLink>
     </MainSection>
   )
 }
