4c0ae7dc32ed8f1bd72fc1cecdbc0e324c2b4b80
AntoniaGranit <antonia.granit@gmail.com>
2023-07-19 10:23:17 +0300

    add declare module files

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,2 +1,1 @@
 declare module '*';
-declare module 'react-dom/client';
--- src/react-dom.d.ts
+++ src/react-dom.d.ts
@@ -1,4 +1,1 @@
-declare module 'react-dom/client' {
-    export function createRoot(container: Element | null, options?: { hydrate?: boolean }): ReactRoot;
-    // Add other declarations as needed for your specific use case.
-  }
+declare module 'react-dom/client';
